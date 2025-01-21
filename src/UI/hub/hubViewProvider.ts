d2b73d13fff59fb82971ab7711ceeff02354f0c3
Daniel Flick <flickd@students.uni-marburg.de>
2025-01-21 16:33:38 +0100

    add files button functionality implemented, frontend styles improved

--- src/UI/hub/hubViewProvider.ts
+++ src/UI/hub/hubViewProvider.ts
@@ -20,6 +20,7 @@ export class HubViewProvider implements vscode.WebviewViewProvider {
 
   private savedFileContent: string = '';
   private decorationType: vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType({});
+  private selectedFiles: string[] = [];
 
   constructor(
     private readonly _extensionUri: vscode.Uri, 
@@ -54,7 +55,13 @@ export class HubViewProvider implements vscode.WebviewViewProvider {
     webviewView.webview.onDidReceiveMessage(async message => {
       console.log('Received message:', message);
       if (message.command === 'commentFile') {
-
+        const files = message.files;
+        for (const file of files) {
+          const document = await vscode.workspace.openTextDocument(file);
+          const content = document.getText();
+          console.log('File content:', content);
+          // Process the content as needed
+        }
         const editor = vscode.window.activeTextEditor;
         if (editor) {
           const document = editor.document;
@@ -117,6 +124,31 @@ export class HubViewProvider implements vscode.WebviewViewProvider {
 
           webviewView.webview.postMessage({ command: 'hideAcceptDismissButtons' });
         }
+      } else if (message.command === 'addFiles') {
+        const f = vscode.workspace.workspaceFolders;
+        let g: string = '';
+        if (f) {
+          g = f[0].uri.fsPath;
+        }
+        const files = await vscode.window.showOpenDialog({
+          canSelectMany: true,
+          openLabel: 'Select files',
+          filters: {
+            'All files': ['*']
+          }
+        });
+        if (files) {
+          for (const file of files) {
+            if (!this.selectedFiles.includes(file.fsPath) && file.fsPath.startsWith(g)) {
+              const pathWithinWorkspace = file.fsPath.replace(g, '').substring(1);
+              this.selectedFiles.push(pathWithinWorkspace);
+            }
+          }
+          webviewView.webview.postMessage({ command: 'updateFileList', files: this.selectedFiles });
+        }
+      } else if (message.command === 'updateFileList') {
+        this.selectedFiles = message.files;
+        webviewView.webview.postMessage({ command: 'updateFileList', files: this.selectedFiles });
       }
     });
 
@@ -148,30 +180,65 @@ export class HubViewProvider implements vscode.WebviewViewProvider {
         <title>HUB</title>
         <script>
           const vscode = acquireVsCodeApi();
+          let selectedFiles = [];
+
           function commentFile() {
-            vscode.postMessage({ command: 'commentFile' });
+            vscode.postMessage({ command: 'commentFile', files: selectedFiles });
           }
+
           function acceptChanges() {
             vscode.postMessage({ command: 'acceptChanges' });
           }
           function dismissChanges() {
             vscode.postMessage({ command: 'dismissChanges' });
           }
+
+          function addFiles() {
+            vscode.postMessage({ command: 'addFiles' });
+          }
+
           window.addEventListener('message', event => {
             const message = event.data;
             if (message.command === 'showAcceptDismissButtons') {
-              document.getElementById('accept-dismiss-buttons').style.display = 'block';
+              document.getElementById('accept-dismiss-buttons').style.display = 'flex';
             } else if (message.command === 'hideAcceptDismissButtons') {
               document.getElementById('accept-dismiss-buttons').style.display = 'none';
+            } else if (message.command === 'updateFileList') {
+              selectedFiles = message.files;
+              const fileListContainer = document.getElementById('file-list');
+              fileListContainer.innerHTML = '';
+              selectedFiles.forEach(file => {
+                const listItemP = document.createElement('p');
+                listItemP.textContent = file;
+                listItemP.style.padding = '0';
+                listItemP.className = 'file-list-item-p';
+                const removeButton = document.createElement('button');
+                removeButton.textContent = 'X';
+                removeButton.className = 'li-remove-button';
+                removeButton.onclick = () => {
+                  selectedFiles = selectedFiles.filter(f => f !== file);
+                  vscode.postMessage({ command: 'updateFileList', files: selectedFiles });
+                };
+                const listItem = document.createElement('li');
+                listItem.className = 'file-list-item';
+                listItem.appendChild(listItemP);
+                listItem.appendChild(removeButton);
+                fileListContainer.appendChild(listItem);
+              });
             }
           });
         </script>
       </head>
       <body>
-        <button onclick="commentFile()">Comment file</button>
-        <div id="accept-dismiss-buttons" style="display: none;">
-          <button onclick="acceptChanges()">Accept</button>
-          <button onclick="dismissChanges()">Dismiss</button>
+        <div id="comment-file-container">
+          <h1 class="top-headline">Select Files to comment</h1>
+          <button onclick="addFiles()">+ Add files...</button>
+          <ul id="file-list" style="padding: 0;"></ul>
+          <button onclick="commentFile()">Comment Files</button>
+          <div id="accept-dismiss-buttons" style="display: none; flex-direction: row; ">
+            <button class="accept-button" style="margin-right: 5px;" onclick="acceptChanges()">Accept</button>
+            <button class="dismiss-button" onclick="dismissChanges()">Dismiss</button>
+          </div>
         </div>
       </body>
       </html>
