09854c0e12ba5ce0fb1ae058af6f94befef099c7
Haye <aahmad@daenet.com>
2025-02-27 18:08:15 +0100

    removed not needed code/images

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,11 @@
 import * as vscode from 'vscode';
-import { displayHUBPrimarySidebar } from './UI/hub/hubViewProvider';
 import { AutoCommenter } from './AutoCommenter';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// displays the HUB view in the Primary Sidebar
-	//displayHUBPrimarySidebar(context);
-
 	new AutoCommenter(context);
-
+	
 }
-
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
